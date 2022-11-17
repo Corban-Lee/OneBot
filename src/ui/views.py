c233f90718f0a712029f2d99ded681ec0f60a18e
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-11-17 21:45:00 +0000

    made music buttons functional

--- src/ui/views.py
+++ src/ui/views.py
@@ -24,25 +24,57 @@ class TrackAddedView(dui.View):
     """View for the track added message, has controls for managing the 
     added track in the queue"""
 
-    __slots__ = ()
+    __slots__ = ("song", "voice_state")
 
     def __init__(self, song, voice_state):
         super().__init__(timeout=300)
         self.song = song
         self.voice_state = voice_state
 
-    @dui.button(label="Play Now", style=ButtonStyle.green)
+    @dui.button(
+        label="Play Now",
+        style=ButtonStyle.primary
+    )
     async def play_now(self, inter:Inter, button:dui.Button):
-        await inter.response.send_message("dummy")
+        """Skip the queue and play the added track now"""
 
-    @dui.button(label="Remove from Queue", style=ButtonStyle.red)
+        # Lock this command to users with elevated permissions
+        permissions = inter.channel.permissions_for(inter.user)
+        if discord.Permissions(moderate_members=True) not in permissions:
+            return await inter.response.send_message(
+                "You don't have permission to do that",
+                ephemeral=True
+            )
+
+        # Ensure that the song is not already playing
+        if self.voice_state.current == self.song:
+            return await inter.response.send_message(
+                "This song is already playing",
+                ephemeral=True
+            )        
+
+        await inter.response.send_message(
+            "Skipping queue and playing now..."
+        )
+
+        # Skip the queue and play the song now
+        self.voice_state.skip_to_song(
+            self.voice_state.queue.index(self.song)
+        )
+
+
+    @dui.button(
+        label="Remove from Queue",
+        style=ButtonStyle.secondary
+    )
     async def remove_from_queue(self, inter:Inter, button:dui.Button):
         await inter.response.send_message("dummy")
+        if inter.user != self.voice_state.current.requester:
+            return await inter.response.send_message(
+                "You are not the requester of this song",
+                ephemeral=True
+            )
 
-    @dui.button(label="Get Media Controls", style=ButtonStyle.secondary)
-    async def get_media_controls(self, inter:Inter, button:dui.Button):
-        await inter.response.send_message("dummy")
-    
 
 class MusicControlView(dui.View):
     """View for music control buttons"""
@@ -54,24 +86,99 @@ class MusicControlView(dui.View):
     # 2nd row: mute/unmute, volume down, volume up
     # 3rd row: loop, stop, shuffle
 
-    def __init__(self, song_id: int):
+    def __init__(self, voice_state):
         super().__init__(timeout=300)
-        self.song_id = song_id
+        self.voice_state = voice_state
+        self.song = voice_state.current
 
-    @dui.button(label="⏮️ Rewind", style=ButtonStyle.secondary)
+    def ensure_current_song(self):
+        """Ensures that the current song is the same as the one that the 
+        view was created with"""
+        if self.song != self.voice_state.current:
+            raise ValueError(
+                "The current song has changed, please use the new "
+                "controls for that song"
+            )
+
+    @dui.button(
+        emoji="⏮️",
+        style=ButtonStyle.secondary,
+        row=0
+    )
     async def rewind(self, inter:Inter, button:dui.Button):
+        self.ensure_current_song()
         await inter.response.send_message("dummy")
 
-    @dui.button(label="⏯️ Play/Pause", style=ButtonStyle.secondary)
+    @dui.button(
+        emoji="⏯️",
+        style=ButtonStyle.secondary,
+        row=0
+    )
     async def pause_resume(self, inter:Inter, button:dui.Button):
+        self.ensure_current_song()
         await inter.response.send_message("dummy")
 
-    @dui.button(label="⏭️ Forward", style=ButtonStyle.secondary)
+    @dui.button(
+        emoji="⏭️",
+        style=ButtonStyle.secondary,
+        row=0
+    )
     async def forward(self, inter:Inter, button:dui.Button):
+        self.ensure_current_song()
         await inter.response.send_message("dummy")
 
-    @dui.button(label="⏹️ Stop", style=ButtonStyle.secondary)
+    @dui.button(
+        emoji="🔇",
+        style=ButtonStyle.secondary,
+        row=1
+    )
+    async def mute(self, inter:Inter, button:dui.Button):
+        self.ensure_current_song()
+        await inter.response.send_message("dummy")
+
+    @dui.button(
+        emoji="🔉",
+        style=ButtonStyle.secondary,
+        row=1
+    )
+    async def volume_down(self, inter:Inter, button:dui.Button):
+        self.ensure_current_song()
+        await inter.response.send_message("dummy")
+
+    @dui.button(
+        emoji="🔊",
+        style=ButtonStyle.secondary,
+        row=1
+    )
+    async def volume_up(self, inter:Inter, button:dui.Button):
+        self.ensure_current_song()
+        await inter.response.send_message("dummy")
+
+    @dui.button(
+        emoji="🔁",
+        style=ButtonStyle.secondary,
+        row=2
+    )
+    async def loop(self, inter:Inter, button:dui.Button):
+        self.ensure_current_song()
+        await inter.response.send_message("dummy")
+
+    @dui.button(
+        emoji="⏹️",
+        style=ButtonStyle.secondary,
+        row=2
+    )
     async def stop(self, inter:Inter, button:dui.Button):
+        self.ensure_current_song()
+        await inter.response.send_message("dummy")
+
+    @dui.button(
+        emoji="🔀",
+        style=ButtonStyle.secondary,
+        row=2
+    )
+    async def shuffle(self, inter:Inter, button:dui.Button):
+        self.ensure_current_song()
         await inter.response.send_message("dummy")
 
 ###################################################################################
