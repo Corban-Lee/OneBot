c25d7db111cf247656a259646602731c39089cc0
Corban-Lee <77944149+XordK@users.noreply.github.com>
2022-11-17 21:44:03 +0000

    removed useless pass statements

--- src/exceptions.py
+++ src/exceptions.py
@@ -3,12 +3,9 @@
 
 class EmptyQueryResult(Exception):
     """The query returned no results"""
-    pass
 
 class VoiceError(Exception):
     """An error occured in the voice client"""
-    pass
 
 class YTDLError(Exception):
     """An error occured while fetching data from YouTube"""
-    pass
